4d00b9212ecb65213cc5d15a9e95cde5cc47b049
KeswetRinret <rkeswety@gmail.com>
2024-08-01 22:30:28 -0700

    marb store page responsiveness

--- src/components/myWorks/Marb.jsx
+++ src/components/myWorks/Marb.jsx
@@ -6,7 +6,7 @@ import Footer from "../Footer";
 
 const Marb = () => {
   return (
-    <div className=" mx-32 my-4">
+    <div className="md:mx-32 mx-4 md:my-4">
       <div className="w-full flex flex-col justify-center items-center space-y-4">
         <Link to={"/"}>
           <img
@@ -19,23 +19,23 @@ const Marb = () => {
 
       <div className="flex items-center justify-center mt-8">
         <img
-          className="object-cover h-[600px] w-dvw rounded-xl "
+          className="object-cover h-64 md:h-[600px] md:w-dvw w-full rounded-xl "
           // src={Laptop}
           src="https://framerusercontent.com/images/ZuDzZNFD4y4ZbZ3Cf9QFDB0vGs.jpg?scale-down-to=2048"
           alt="Marb Store"
         />
       </div>
 
-      <div className="mt-16">
+      <div className="md:mt-16 mt-12">
         <div className="">
-          <p className="font-semibold text-4xl font-tetiaryFont">E-Commerce</p>
+          <p className="font-semibold md:text-4xl text-2xl font-tetiaryFont">E-Commerce</p>
         </div>
-        <hr className="mt-6" />
+        <hr className="md:mt-6 mt-4" />
 
-        <div className="flex gap-20 mt-8">
-          <div className="w-[60%]">
-            <p className="text-2xl font-semibold font-secondaryFont">Intro</p>
-            <p className="text-xl mt-6 leading-8">
+        <div className="flex md:gap-20 md:flex-row flex-col md:mt-8 mt-6">
+          <div className="md:w-[60%] w-full">
+            <p className="md:text-2xl text-xl font-semibold font-secondaryFont">Intro</p>
+            <p className="md:text-xl text-lg mt-6 md:leading-8 leading-7">
               The "Marb-store" design project epitomizes the fusion of modern
               aesthetics and functionality where craftsmanship meets elegance in
               the realm of bag making. In this case study, we delve into the
@@ -49,10 +49,10 @@ const Marb = () => {
               to success in the competitive ecommerce landscape.
             </p>
           </div>
-          <div className="w-[40%] space-y-20 ">
+          <div className="md:w-[40%] md:space-y-20 space-y-8 mt-6 ml-1 ">
             <div>
               <p className="text-2xl font-semibold font-secondaryFont ">Year</p>
-              <p className="mt-6 font-medium">2024</p>
+              <p className="md:mt-6 mt-2 font-medium">2024</p>
               <hr className="mt-2" />
             </div>
 
@@ -76,13 +76,13 @@ const Marb = () => {
       </div>
 
       <div className="mt-16">
-        <p className="mt-8 text-5xl font-tertiaryFont font-semibold ">
+        <p className="md:mt-8 md:text-5xl text-3xl font-tertiaryFont font-semibold ">
           Process
         </p>
 
-        <hr className="mt-6" />
+        <hr className="md:mt-6 mt-4" />
 
-        <p className="text-xl mt-8 leading-8 ">
+        <p className="md:text-xl text-lg md:mt-8 mt-6 md:leading-8 leading-7">
           The "Marb-store" design process is a streamlined yet dynamic approach,
           beginning with in-depth research to understand client needs and user
           behaviors. Creativity blooms in the conceptualization phase, where
@@ -94,26 +94,26 @@ const Marb = () => {
           into the cycle, ensuring the brand's enduring resonance and relevance.
         </p>
       </div>
-      <div className="flex items-center justify-center mt-16">
+      <div className="flex items-center justify-center md:mt-16 mt-8">
         <img
-          className="object-cover h-[600px] w-dvw rounded-xl"
+          className="object-cover h-64 md:h-[600px] w-dvw rounded-xl"
           src="https://framerusercontent.com/images/RcPneXyIYHylJg9O5zIYAFkaYw.jpg?scale-down-to=2048"
           alt="Marb Store"
         />
       </div>
 
-      <div className="flex items-center justify-center mt-16">
+      <div className="flex items-center justify-center md:mt-16 mt-8">
         <img
-          className=" object-cover h-[600px] w-dvw rounded-xl "
+          className=" object-cover h-64 md:h-[600px] w-dvw rounded-xl "
           src="https://framerusercontent.com/images/bVFqO2djh4Z4ijH0AzoI2Aozo.jpg"
           alt="Marb Store"
         />
       </div>
 
-      <div className="mt-16">
-        <p className="mt-8 text-5xl font-tertiaryFont font-semibold ">Goal</p>
-        <hr className="mt-8" />
-        <p className="text-xl mt-8 leading-8 ">
+      <div className="md:mt-16 mt-12">
+        <p className="md:mt-8 md:text-5xl text-3xl font-tertiaryFont font-semibold ">Goal</p>
+        <hr className="md:mt-8 mt-4" />
+        <p className="md:text-xl text-lg md:mt-8 mt-6 md:leading-8 leading-7">
           The design process for Marb-store is a strategic and goal-oriented
           approach that begins with a thorough analysis of objectives and target
           outcomes. In the ideation phase, innovation is key as we envision and
@@ -128,28 +128,28 @@ const Marb = () => {
           premium bags and unmatched customer satisfaction.
         </p>
 
-        <div className="flex items-center justify-center mt-16">
+        <div className="flex items-center justify-center md:mt-16 mt-8">
           <img
-            className=" object-cover h-[600px] w-dvw rounded-xl "
+            className=" object-cover h-64 md:h-[600px] w-dvw rounded-xl "
             src="https://framerusercontent.com/images/hzL4ehQKmdkQmUTF814YViCOJ5g.jpg"
             alt="Marb Store"
           />
         </div>
 
-        <div className="flex items-center justify-center mt-16">
+        <div className="flex items-center justify-center md:mt-16 mt-8">
           <img
-            className=" object-cover h-[600px] w-dvw rounded-xl "
+            className=" object-cover h-64 md:h-[600px] w-dvw rounded-xl "
             src="https://framerusercontent.com/images/9PNSzHhfGXSW26rY8kUd4OOHaY.jpg"
             alt="Marb Store"
           />
         </div>
 
-        <div className=" mt-16">
-          <p className="mt-8 text-5xl font-tertiaryFont font-semibold ">
+        <div className=" md:mt-16 mt-12">
+          <p className="md:mt-8 md:text-5xl text-3xl font-tertiaryFont font-semibold ">
             Result
           </p>
-          <hr className="mt-8" />
-          <p className="text-xl mt-8 leading-8 ">
+          <hr className="md:mt-8 mt-4" />
+          <p className="md:text-xl text-lg md:mt-8 mt-6 md:leading-8 leading-7">
             The design process for Marb-store's results-driven approach begins
             with a clear definition of desired outcomes and benchmarks. In the
             brainstorming phase, we explore and refine potential pathways to
@@ -166,55 +166,54 @@ const Marb = () => {
         </div>
       </div>
 
-      <div className="mt-16">{Contact}</div>
-        <div>
-          <p className="text-5xl font-tertiaryFont font-semibold">Contact</p>
+      <div className="md:mt-16 mt-12 ">
+        <div href="contact">
+          <p className="md:text-5xl text-3xl font-tertiaryFont font-semibold">Contact</p>
         </div>
-        <hr className="mt-10" />
+        <hr className="md:mt-10 mt-6" />
 
         <div className="">
-          <div className="mt-12 flex">
-            <p className="w-1/2 text-8xl space-y-8">
+          <div className="md:mt-12 mt-7 flex md:flex-row flex-col">
+            <p className="w-1/2 md:text-8xl text-5xl md:space-y-8">
               Let's <br /> work <br /> together
             </p>
 
-            <p className="w-1/2 text-xl leading-8">
+            <p className="md:w-1/2 w-full font-secondaryFont md:text-xl text-lg md:leading-8 leading-7 mt-4">
               I'm very happy to tackle new challenges and dive into the most
               innovative projects. If you are looking for a dedicated and
               enthusiastic designer to enhance your vision, this is where it
               will end. We'll work together to develop unique experiences that
               will be remembered forever. Contact me, and let's go on an
               exciting journey together to meet your objectives!
-              <div className=" mt-10 text-3xl font-tertiaryFont font-semibold relative group">
-                <span class="absolute bottom-0 left-0 w-0 h-0.5 bg-black group-hover:w-full group-hover:transition-all group-hover:duration-200"></span>
-                hello@mail.com
+              <div className="md:mt-10 mt-4 md:text-3xl text-2xl font-tertiaryFont font-semibold relative group">
+                <span class="absolute bottom-0 left-0 w-0 h-0.5 bg-gray-800 group-hover:w-full group-hover:transition-all group-hover:duration-300"></span>
+                festusandrew23@gmail.com
               </div>
             </p>
           </div>
         </div>
-
-        <div className='mb-10'>
-       <hr className="mt-12" />
-      <footer className="mt-16 flex">
-        <p className="w-[60%] text-xl font-tertiaryFont">
+      </div>
+        <div className='md:mb-10 mb-4'>
+       <hr className="md:mt-12 mt-6" />
+      <footer className="md:mt-16 mt-8 flex md:flex-row flex-col">
+      <p className="md:w-[60%] text-xl md:font-tertiaryFont flex justify-center">
           © 2023 Templify by Patryk Moskot
         </p>
 
-        <div className=" w-[40%] flex justify-end ">
-            <Link to={"./Works"}>
-          <p className="font-primaryFont text-3xl  relative group">
-            Work
-            <span class="absolute bottom-0 left-0 w-0 h-0.5  bg-gray-800 group-hover:w-full group-hover:transition-all group-hover:duration-300"></span>
-          </p>
-          </Link>
+        <div className="md:w-[40%] w-full flex md:justify-end justify-center mt-5">
+          <a href="MyWork2">
+            <p className="font-primaryFont md:text-3xl text-2xl  relative group">
+              Work
+              <span class="absolute bottom-0 left-0 w-0 h-0.5  bg-gray-800 group-hover:w-full group-hover:transition-all group-hover:duration-300"></span>
+            </p>
+          </a>
 
-          <Link to={"./About"}>
-          <p className="text-3xl ml-8 font-primaryFont relative group">
-            
-            About
-            <span class="absolute bottom-0 left-0 w-0 h-0.5  bg-gray-800 group-hover:w-full group-hover:transition-all group-hover:duration-300"></span>
-          </p>
-          </Link>
+          <a href="MoreAbout">
+            <p className="md:text-3xl text-2xl ml-8 font-primaryFont relative group">
+              About
+              <span class="absolute bottom-0 left-0 w-0 h-0.5  bg-gray-800 group-hover:w-full group-hover:transition-all group-hover:duration-300"></span>
+            </p>
+          </a>
         </div>
 
       </footer>
